fe51dd3d8e5b66323d728ffaccc327f0dcd602c2
garcia-jm <garcia.justinmari@gmail.com>
2024-06-12 23:33:00 -0600

    dede

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -1,3 +1,7 @@
-export const POST = async (req: Request) => {
+import { NextResponse } from "next/server";
+
+export const POST = async (req: Request, res: NextResponse) => {
   console.log("I got it!");
+
+  return NextResponse.json({ status: 201 });
 };
