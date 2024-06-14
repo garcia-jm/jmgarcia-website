f7d33752fc2d3c41aebd4478961f954466c8d954
garcia-jm <garcia.justinmari@gmail.com>
2024-06-14 13:11:28 -0600

    dd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import NavBar from "./(root)/_components/NavBar";
@@ -30,6 +31,7 @@ export default function RootLayout({
         <NavBar />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
