3b84e5cd44b1330389c1414f83dcc5324fa2fd42
garcia-jm <garcia.justinmari@gmail.com>
2025-04-28 20:17:26 -0600

    Delete rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
-  async rewrites() {
-    return [
-      {
-        source: "/casecobra/:path*",
-        destination: "https://casecobra-proj.vercel.app/:path*",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
